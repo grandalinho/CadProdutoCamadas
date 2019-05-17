03a46e88c6eeae80c8636f586080182097080544
JONAS NICIOLI DE CARVALHO <jonas.ncarvalho@senacsp.edu.br>
2019-05-17 17:06:46 -0300

    inicio da montagem do window builder

--- src/com/cadproduto/view/GerenciarProdutos.java
+++ src/com/cadproduto/view/GerenciarProdutos.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
 
 public class GerenciarProdutos extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
+	private JTextField txtNome;
+	private JTextField txtDescricao;
+	private JTextField txtFabricante;
+	private JTextField txtQuantidade;
+	private JTextField txtPreco;
 
 	/**
 	 * Launch the application.
@@ -32,11 +43,67 @@ public class GerenciarProdutos extends JFrame {
 	 */
 	public GerenciarProdutos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 642, 490);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 243, 606, 197);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNome.setBounds(10, 11, 67, 32);
+		contentPane.add(lblNome);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(10, 44, 213, 26);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JLabel lblDescrio = new JLabel("Descri\u00E7\u00E3o:");
+		lblDescrio.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDescrio.setBounds(10, 81, 67, 32);
+		contentPane.add(lblDescrio);
+		
+		txtDescricao = new JTextField();
+		txtDescricao.setBounds(10, 124, 213, 108);
+		contentPane.add(txtDescricao);
+		txtDescricao.setColumns(10);
+		
+		JLabel lblFabricante = new JLabel("Fabricante:");
+		lblFabricante.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblFabricante.setBounds(233, 11, 67, 32);
+		contentPane.add(lblFabricante);
+		
+		txtFabricante = new JTextField();
+		txtFabricante.setBounds(233, 44, 196, 26);
+		contentPane.add(txtFabricante);
+		txtFabricante.setColumns(10);
+		
+		JLabel lblQuantidade = new JLabel("Quantidade:");
+		lblQuantidade.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblQuantidade.setBounds(233, 81, 81, 32);
+		contentPane.add(lblQuantidade);
+		
+		txtQuantidade = new JTextField();
+		txtQuantidade.setBounds(233, 124, 96, 26);
+		contentPane.add(txtQuantidade);
+		txtQuantidade.setColumns(10);
+		
+		JLabel lblPreo = new JLabel("Pre\u00E7o:");
+		lblPreo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblPreo.setBounds(233, 162, 81, 32);
+		contentPane.add(lblPreo);
+		
+		txtPreco = new JTextField();
+		txtPreco.setBounds(233, 206, 96, 26);
+		contentPane.add(txtPreco);
+		txtPreco.setColumns(10);
 	}
-
 }
