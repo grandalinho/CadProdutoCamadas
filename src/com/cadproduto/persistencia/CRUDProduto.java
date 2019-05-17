b336b1fb96a314c412c7f0fc3ab9311bee038f9d
JONAS NICIOLI DE CARVALHO <jonas.ncarvalho@senacsp.edu.br>
2019-05-17 17:05:54 -0300

    inicio da montagem do window builder

--- src/com/cadproduto/persistencia/CRUDProduto.java
+++ src/com/cadproduto/persistencia/CRUDProduto.java
@@ -1,5 +1,22 @@
 package com.cadproduto.persistencia;
 
-public class CRUDProduto {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class CRUDProduto {
+	
+	private Connection con = null;
+	private ResultSet rs = null;
+	private PreparedStatement pst = null;
+	
+	
+	Public String cadastrar(Produto Produto) {
+		
+		String msg = "";
+		
+		try {
+			
+		}
+	}
 }
--- src/com/cadproduto/view/GerenciarProdutos.java
+++ src/com/cadproduto/view/GerenciarProdutos.java
@@ -1,5 +1,42 @@
 package com.cadproduto.view;
 
-public class GerenciarProdutos {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class GerenciarProdutos extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GerenciarProdutos frame = new GerenciarProdutos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GerenciarProdutos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
 
 }
